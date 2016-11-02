3637d33ec36ffe8aa8b665c27568f3acbb407e9b
olafurj <olafurj@olafurj.tern.is>
2016-11-02 11:33:48 +0000

    tempaltes

--- subscriber.py
+++ subscriber.py
@@ -1,8 +1,28 @@
+from threading import Thread
+import zmq, socketio, eventlet
+from flask import Flask, render_template
 
-import zmq
+sio = socketio.Server()
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@sio.on('connect')
+def connect(socket_id, environment):
+    print 'connect sid: %s, env: %s' % (socket_id, environment)
+
+@sio.on('message')
+def message(socket_id, data):
+    print 'message sid: %s, data: %s' % (socket_id, data)
+
+@sio.on('disconnect')
+def disconnect(socket_id):
+    print 'disconnect sid: %s' % socket_id
 
 
-def main():
+def init_zeromq():
 
     print 'zmq version %s' % zmq.zmq_version()
 
@@ -17,5 +37,16 @@ def main():
     subscriber.close()
     context.term()
 
-if __name__ == "__main__":
-    main()
+
+if __name__ == '__main__':
+
+    # start 0mq, it blocks so we start a thread
+    t = Thread(target=init_zeromq)
+    t.start()
+
+    # start flask
+    app = socketio.Middleware(sio, app)
+    eventlet.wsgi.server(eventlet.listen(('', 8000)), app)
+
+    t.join()
+
